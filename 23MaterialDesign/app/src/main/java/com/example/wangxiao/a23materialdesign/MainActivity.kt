afab96d09047ac3e03da72a046d2832b585bcfd6
Jerrywx <23601145@qq.com>
2017-06-16 01:37:06 +0800

    添加 ToolBar

--- 23MaterialDesign/app/src/main/java/com/example/wangxiao/a23materialdesign/MainActivity.kt
+++ 23MaterialDesign/app/src/main/java/com/example/wangxiao/a23materialdesign/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.example.wangxiao.a23materialdesign
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.support.v7.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /// toolbar
+        val toolBar: Toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolBar)
+
+        /// 按钮
+        val toolbar = findViewById(R.id.open_toolbar) as Button
+        toolbar.setOnClickListener {
+            val intent = Intent(this, ToolBarActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+//        return super.onOptionsItemSelected(item)
+
+        when (item?.itemId) {
+
+            R.id.backup ->
+                    Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+            R.id.delete ->
+                Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+            R.id.settings ->
+                Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+            else ->
+                    return false
+        }
+
+        return true
+    }
+
 }
---
+++ 23MaterialDesign/app/src/main/java/com/example/wangxiao/a23materialdesign/ToolBarActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wangxiao.a23materialdesign
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ToolBarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tool_bar)
+    }
+}
