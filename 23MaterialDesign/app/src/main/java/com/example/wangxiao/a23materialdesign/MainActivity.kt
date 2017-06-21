903a432ff0514218c8526c1e0875a319dafb69bc
Jerrywx <23601145@qq.com>
2017-06-21 23:25:04 +0800

    添加menu 菜单

--- 23MaterialDesign/app/src/main/java/com/example/wangxiao/a23materialdesign/MainActivity.kt
+++ 23MaterialDesign/app/src/main/java/com/example/wangxiao/a23materialdesign/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.wangxiao.a23materialdesign
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
 import android.widget.Button
 import android.support.v7.widget.Toolbar
 import android.view.Menu
@@ -11,6 +13,8 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    var mDrawerLayout: DrawerLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,6 +22,14 @@ class MainActivity : AppCompatActivity() {
         /// toolbar
         val toolBar: Toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolBar)
+
+        ///
+        mDrawerLayout = findViewById(R.id.drawer_layout) as DrawerLayout
+        var actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
 
         /// 按钮
         val toolbar = findViewById(R.id.open_toolbar) as Button
@@ -28,26 +40,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        return super.onCreateOptionsMenu(menu)
         menuInflater.inflate(R.menu.toolbar, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-//        return super.onOptionsItemSelected(item)
-
         when (item?.itemId) {
-
             R.id.backup ->
                     Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
             R.id.delete ->
                 Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
             R.id.settings ->
                 Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+            android.R.id.home ->
+                mDrawerLayout?.openDrawer(GravityCompat.START)
             else ->
                     return false
         }
-
         return true
     }
 
